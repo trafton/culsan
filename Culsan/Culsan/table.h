e1902b4a1b64fb6f74cc4e7afd492afed892308a
Trafton Haynes <trafton@MeaCulpa.local>
2023-06-08 15:26:34 -0700

    hashtables

---
+++ Culsan/Culsan/table.h
@@ -0,0 +1,32 @@
+//
+//  table.h
+//  Culsan
+//
+//  Created by Trafton Haynes on 6/5/23.
+//
+
+#ifndef table_h
+#define table_h
+
+#include "common.h"
+#include "value.h"
+
+typedef struct {
+    ObjString* key;
+    Value value;
+} Entry;
+
+typedef struct {
+    int count;
+    int capacity;
+    Entry* entries;
+} Table;
+
+void initTable(Table* table);
+void freeTable(Table* table);
+bool tableSet(Table* table, ObjString* key, Value value);
+void tableAddAll(Table* from, Table* to);
+bool tableGet(Table* table, ObjString* key, Value* value);
+bool tableDelete(Table* table, ObjString* key);
+
+#endif /* table_h */
